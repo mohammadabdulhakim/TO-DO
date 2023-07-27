016bfe21f02e2234edf357910a0c5370ce5f992f
duxpixels <duxwork0@gmail.com>
2023-07-27 14:55:41 +0300

    adding border

--- src/components/inputs/Textarea.jsx
+++ src/components/inputs/Textarea.jsx
@@ -18,9 +18,9 @@ errors,
         <TextareaAutosize minRows={5} maxRows={9} id={id} disabled={disabled} required {...register(id)} placeholder=" " className={`
         resize-none
         bg-second-bg dark:bg-second-dark-bg
-        peer w-full p-4 pt-6 font-normal border-none rounded-md outline-none focus:outline-none transition disabled:opacity-70 disabled:cursor-not-allowed pl-4 
+        peer w-full p-4 pt-6 font-normal border-2 rounded-md outline-none focus:outline-none transition disabled:opacity-70 disabled:cursor-not-allowed pl-4 
         ${errors[id]? "border-rose-500":"border-neutral-300"}
-        ${errors[id]? "focus:border-rose-500":"focus:border-black"}
+        ${errors[id]? "focus:border-rose-500":"focus:border-neutral-400"}
         `} />
 
         <label className={`
